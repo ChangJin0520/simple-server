830654926beb72e74ff6a6c2580ff240483d008f
jin.chang <jin.chagn@qiyuesuo.com>
2022-02-27 14:16:02 +0800

    feat: 添加 /api/login

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const Koa = require('koa');
-const app = new Koa();
+const Router = require('@koa/router');
+const bodyParser = require('koa-bodyparser');
 
-app.use(async ctx => {
-    ctx.body = 'Hello World';
-});
+const app = new Koa();
+const router = new Router();
+
+app.use(bodyParser());
+
+router.post('/api/login', (ctx, next) => {
+    const { request } = ctx;
+    const { body } = request;
+    const { name, password } = body;
+    
+    console.log(`用户名：${name}；密码：${password}`);
+
+    ctx.body = {
+        code: 200,
+        message: '登陆成功',
+        result: {
+            token: '123456'
+        }
+    }
+
+    next();
+}),
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods())
 
 app.listen(3000);
